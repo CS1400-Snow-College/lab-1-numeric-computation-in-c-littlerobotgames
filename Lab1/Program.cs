4aa37423c8daa35ef37900bf1c9dd885965c039c
Little Robot Games <76882552+littlerobotgames@users.noreply.github.com>
2025-09-03 09:54:59 -0600

    Steps 2 - 4

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,5 +1,45 @@
-﻿int a = 18;
+﻿//Step 1
+int a = 18;
 int b = 6;
 int c = a + b;
-
 Console.WriteLine(c);
+
+c = a - b;
+Console.WriteLine(c);
+
+//Step 2
+a = 5;
+b = 4;
+c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+
+d = (a + b) * c;
+Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+a = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+Console.WriteLine(d);
+
+//Step 3
+a = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"an example of overflow: {what}");
+
+//Step 4
